f97a88748974f6ecfbd4db836a064aa5fe971f3a
David Su <davidsu@geotab.com>
2020-01-10 09:28:42 -0500

    Bump version: 0.1.8 → 0.1.9

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/lucidsushi/gs_pip_install',
-    version='0.1.8',
+    version='0.1.9',
     zip_safe=False,
 )
