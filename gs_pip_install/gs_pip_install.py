8702d6618a02be8ff92e8888efae3e61cc30c484
saltyseadawg <sabrinayu7@gmail.com>
2021-03-04 16:07:49 -0500

    refactor gs_pip_install to not use gsutil

--- gs_pip_install/gs_pip_install.py
+++ gs_pip_install/gs_pip_install.py
@@ -3,20 +3,19 @@ import subprocess
 import sys
 
 import click
+from google.cloud import storage
 
 
-@click.command()
-@click.option("--bucket_url", help="(str) gs://some-bucket")
-@click.option("--r", default="", help="(str) Path to gs_requirements.txt")
-@click.option("--package_name", default="", help="(str) Name of Python package")
+@click.option("--bucket_name", help="(str) gs://some-bucket")
+@click.option("--req_file", default="", help="(str) Name of Python requirements file")
 @click.option(
     "--target_dir", default="", help="(str) Directory to install package into"
 )
-def main(bucket_url, package_name, r, target_dir):
-    """Pip install {bucket_url}/{pkg_name}/{pkg_name_versioned}.tar.gz to
+@click.option("--project_id", help="(str) Name of GCP project")
+@click.option("--download_dir", default="")
+def main(bucket_name, req_file, target_dir, project_id, download_dir):
+    """Pip install {pkg_name}/{pkg_name_versioned}.tar.gz to
     current enviroment or a target directory
-
-    (0) "gsutil" command line tool must exist
 
     (1) Copy package_name.tar.gz from Google Cloud bucket
 
@@ -24,69 +23,83 @@ def main(bucket_url, package_name, r, target_dir):
 
     (3) Remove the package_name.tar.gz
     """
-    install_commands = []
-    if r:
-        with open(r) as gs_requirements:
-            for package_name in gs_requirements.readlines():
-                install_commands.append(
-                    form_install_command(
-                        bucket_url, package_name.rstrip("\n"), target_dir
-                    )
-                )
-    else:
-        install_commands.append(
-            form_install_command(bucket_url, package_name.rstrip("\n"), target_dir)
-        )
-    for install_command in install_commands:
-        gs_pip_install(install_command)
-
-
-def form_install_command(bucket_url, package_name="", target_dir=""):
-
-    pkg_name_versioned = package_name.replace("==", "-")
-    pkg_name_clean = package_name.split("==")[0]
-
-    gs_package_path = "{bucket_url}/{pkg_name}/{pkg_name_versioned}.tar.gz".format(
-        bucket_url=bucket_url,
-        pkg_name=pkg_name_clean,
-        pkg_name_versioned=pkg_name_versioned,
+    download_packages(
+        bucket_name=bucket_name,
+        project_id=project_id,
+        gs_requirements_file=req_file,
+        packages_download_dir=download_dir
     )
-    gs_package_name = os.path.basename(gs_package_path)
+    install_packages(download_dir,target_dir)
 
-    if target_dir:
-        pip_install = [
-            sys.executable,
-            "-m",
-            "pip",
-            "install",
-            "--quiet",
-            "--no-deps",
-            "-t",
-            target_dir,
-            gs_package_name,
-        ]
-    else:
-        pip_install = [
-            sys.executable,
-            "-m",
-            "pip",
-            "install",
-            "--quiet",
-            gs_package_name,
-        ]
+def install_packages(packages_download_dir, target_dir):
+    """[summary]
 
-    return {
-        "install_command": pip_install,
-        "name": gs_package_name,
-        "path": gs_package_path,
-    }
+    :param packages_download_dir: [description]
+    :type packages_download_dir: [type]
+    """
+    for gs_package_zip_file in os.listdir(packages_download_dir):
+        if target_dir:
+            install_command = [
+                sys.executable,
+                "-m",
+                "pip",
+                "install",
+                "--quiet",
+                f"{packages_download_dir}/{gs_package_zip_file}"
+            ]
+        else:
+            install_command = [
+                sys.executable,
+                "-m",
+                "pip",
+                "install",
+                "--quiet",
+                "--no-deps",
+                "-t",
+                target_dir,
+                f"{packages_download_dir}/{gs_package_zip_file}"
+            ]
+        try:
+            subprocess.check_output(install_command)
+        except:
+            logging.warning("Attempting pip install with pyenv python")
+            install_command[0] = f"{os.environ['HOME']}/.pyenv/shims/python"
+            subprocess.check_output(install_command)
+
+    shutil.rmtree(packages_download_dir)
 
 
-def gs_pip_install(install_command):
+def download_packages(
+    packages_download_dir: str,
+    gs_requirements_file="requirements/requirements_google_storage.txt",
+    bucket_name: str,
+    project_id: str
+):
+    """[summary]
 
-    for cmd in (
-        ["gsutil", "-q", "cp", install_command["path"], "."],
-        install_command["install_command"],
-        ["rm", "-f", install_command["name"]],
-    ):
-        subprocess.call(cmd)
+    :param packages_download_dir: [description]
+    :type packages_download_dir: [type]
+    :param gs_requirements_file: [description], defaults to "requirements/requirements_google_storage.txt"
+    :type gs_requirements_file: str, optional
+    :param bucket_name: [description]
+    :type bucket_name: str, optional
+    """
+    # gs_packages_destination = os.path.join(packages_download_dir)
+    os.mkdir(packages_download_dir)
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+
+    with open(gs_requirements_file) as gs_requirements:
+        for package_name in gs_requirements.readlines():
+            package_name = package_name.rstrip("\n")
+
+            name_no_version = package_name.split("==")[0]
+            name_versioned = package_name.replace("==", "-")
+
+            package_filepath = f"{name_versioned}.tar.gz"
+            gs_package_path = f"{name_no_version}/{package_filepath}"
+
+            blob_package = bucket.blob(gs_package_path)
+            blob_package.download_to_filename(os.path.join(packages_download_dir, package_filepath))
+            # subprocess.call(["gsutil", "cp", gs_package_path, gs_packages_destination])
